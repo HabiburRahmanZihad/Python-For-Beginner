b9051cf8fda6afd6837fbd3fb47357883cee06ba
Habibur Rahman Zihad <e241024@ugrad.iiuc.ac.bd>
2025-09-19 17:36:17 +0600

    Formatting data in string

---
+++ String_Format.py
@@ -0,0 +1,15 @@
+num1 = 42
+num2 = 45
+
+name = "Habib"
+
+print('This is my super number:', num1 + num2) # old way of formatting
+
+print(f'This is my super number: {num1 + num2}' ) # new way of formatting (f-string)
+
+
+print('My name is : ', name )
+
+print(f'My name is : {name}' )
+
+print(f'My name is : {name} and my super number is: {num1 + num2}' )
